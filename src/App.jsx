61abe3db41515a292fe11070bb85cf619b0702eb
Amumug <fabriali4@gmail.com>
2024-05-08 11:42:36 +0100

    counter-app

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
           Count
         </button>
-      </div>
+      </div
     </>
   );
 }
