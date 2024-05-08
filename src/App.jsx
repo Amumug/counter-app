6251df74b5cdc4be70b51bbf196a26568af4e7c2
Amumug <fabriali4@gmail.com>
2024-05-08 11:40:13 +0100

    counter-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 export default function App() {
   return (
     <>
-    <div className="flex flex-row min-h-screen justify-center items-center space-x-4z">
-      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-        Count
-      </button>
-      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-        Count
-      </button>
-    </div>
+      <div className="flex flex-row min-h-screen justify-center items-center space-x-4z">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Count
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Count
+        </button>
+      </div>
     </>
   );
 }
