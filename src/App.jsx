bcfdbf9d46dc59b1e356c2974b2202d377434922
Amumug <fabriali4@gmail.com>
2024-05-09 09:45:40 +0100

    counter-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
+
 export default function App() {
+  const [counter, setCounter] = useState(0)
   return (
     <>
-      <div className="flex flex-row min-h-screen justify-center items-center space-x-4z">
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-          Count
+      <div className="flex flex-column min-h-screen justify-center items-center space-x-4">
+          {counter}
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={() => setCounter(counter + 1)}>
+          Increment
         </button>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-          Count
+        <button className="bg-blue-800 hover:bg-green-200 text-white font-mono py-2 px-4 border border-cyan-600 rounded-sm" onClick={() => setCounter(counter - 1)}>
+          Dicrement
         </button>
-      </div
+      </div>
     </>
   );
 }
